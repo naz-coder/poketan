1e3ae726abb8db3260f6f2594fb09365aaeca42e
naz-coder <favouritenaz@yahoo.com>
2023-03-23 10:41:27 +0100

    API data filter

--- src/components/tanList/TanList.jsx
+++ src/components/tanList/TanList.jsx
@@ -4,75 +4,33 @@ import {TanListWrap} from '../tanList/TanListStyle';
 import tan1 from '../../assets/tan1.png'
 
 const TanList = () => {
-  const [tans, setTans] = useState([]);
-  const tansHandler = () => {
-    axios.get("https://pokeapi.co/api/v2/pokemon/").then((response) =>{
-      console.log(response)
-      setTans(response.data.results[0])
+  const [pagedata, setPagedata] = useState([]);
+
+  axios.get("https://pokeapi.co/api/v2/pokemon/").then((response) =>{
+      console.log(pagedata);
+      setPagedata(response.data.results);
     })
-  }
   
   return (
     <React.Fragment>
       <TanListWrap>
-        {/* <div className='col-6 col-md-3'> */}
         <div className="">
-          <button onClick={tansHandler}>Poke</button>
-          {tans}
         <div className='row'>
-          <div className='col-6 col-md-3'>
+          {pagedata.map((data, index) => (
+            <div className='col-6 col-md-3' key={index}>
             <div className='tan-card'>
                 <div className='tan-image'>
                   <img src={tan1}/>
                 </div>
-                <div className='tan-number'>#0001</div>
-                <div className='tan-name'>Bulbasaur</div>
+                <div className='tan-number'>#{index}</div>
+                <div className='tan-name'>{data.name}</div>
                 <div className='card-button'>
                     <button className='button-1'>Grass</button>
                     <button className='button-2'>Poison</button>
                 </div>
             </div>
           </div>
-          <div className='col-6 col-md-3'>
-            <div className='tan-card'>
-                <div className='tan-image'>
-                  <img src={tan1}/>
-                </div>
-                <div className='tan-number'>#0001</div>
-                <div className='tan-name'>Bulbasaur</div>
-                <div className='card-button'>
-                    <button className='button-1'>Grass</button>
-                    <button className='button-2'>Poison</button>
-                </div>
-            </div>
-          </div>
-          <div className='col-6 col-md-3'>
-            <div className='tan-card'>
-                <div className='tan-image'>
-                  <img src={tan1}/>
-                </div>
-                <div className='tan-number'>#0001</div>
-                <div className='tan-name'>Bulbasaur</div>
-                <div className='card-button'>
-                    <button className='button-1'>Grass</button>
-                    <button className='button-2'>Poison</button>
-                </div>
-            </div>
-          </div>
-          <div className='col-6 col-md-3'>
-            <div className='tan-card'>
-                <div className='tan-image'>
-                  <img src={tan1}/>
-                </div>
-                <div className='tan-number'>#0001</div>
-                <div className='tan-name'>Bulbasaur</div>
-                <div className='card-button'>
-                    <button className='button-1'>Grass</button>
-                    <button className='button-2'>Poison</button>
-                </div>
-            </div>
-          </div>
-          
+          ))}          
         </div>
         </div>
       </TanListWrap>
