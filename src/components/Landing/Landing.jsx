30c7381fdf9f3ed6d0f7bb6312b2fee9df6e2c35
naz-coder <favouritenaz@yahoo.com>
2023-04-03 08:49:18 +0100

    Search Functionality Updated

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import axios from 'axios';
 import {AppModalOverlay} from './LandingStyle'
 import mainLogo from '../../assets/mainLogo.png'
@@ -8,13 +8,17 @@ import TanList from "../tanList/TanList";
 
 const Landing = () => {
   const [pagedata, setPagedata] = useState([]);
-  // const [query, setQuery] = useState(" ");
-  const [store, setStore] = useState({query: ' ', list: []});
+  const [store, setStore] = useState({query: '', list: []});
 
+ useEffect(() => {
   axios.get("https://pokeapi.co/api/v2/pokemon/").then((response) =>{
-      console.log(pagedata);
-      setPagedata(response.data.results);
-    })
+    console.log(pagedata);
+    setPagedata(response.data.results);
+    setStore({...store, list: response.data.results})
+  }).catch((error) => {
+    console.log(error);
+  })
+ }, [])
 
     // Search function which updates the state anytime a user types inside the search bar
   const searchHandler = (e) => {
@@ -26,7 +30,6 @@ const Landing = () => {
           query: e.target.value,
           list: results
       })
-    // setQuery(e.target.value);
   } 
 
   return (
@@ -69,7 +72,6 @@ const Landing = () => {
                 : store.list.map(data => {
                   return (
                   <div>
-                    {/* <li key={data.name}>{data.name}</li> */}
                   <div className="">
                   {store.list.map((data, index) => (
                   <div className=''>
@@ -86,19 +88,6 @@ const Landing = () => {
                 }))}
               </ul>
           </div>
-          
-          {/* <div className="row">
-          {store.list.map((data, index) => (
-            <div className='col-6 col-md-3'>
-              
-            <TanList 
-              keyProps={index}
-              indexProps={index}
-              nameProps={data.name}
-            />
-            </div>
-          ))}
-          </div> */}
         </div>
       </AppModalOverlay>
     </React.Fragment>
@@ -106,128 +95,3 @@ const Landing = () => {
 }
 
 export default Landing;
-
-// return (
-//   <React.Fragment>
-//     <AppModalOverlay>
-//       <div className='modal-container'>
-//         <div className='head-section'>
-//         <div className='app-header'>
-//           <img src={mainLogo} className="app-logo"/> 
-//           <img src={appName} className="app-name"/> 
-//         </div>
-//         {/* <label>Enter Name or Number</label> */}
-//         <div className='poketan-search'>
-//         <input 
-//           type="search" 
-//           placeholder='Pokedex Name or Number' 
-//           value={store.query}
-//           onChange={searchHandler}
-//         />
-//         <button 
-//         // onClick={searchHandler}
-//         >Search</button>
-//         </div>
-//         </div>
-//         <div className='search-result'>
-//             <ul>
-//               {(store.query === "" ? "No match found!" : !store.list.length ? "Your search did not return any result" : store.list.map(data => {
-//                 return <li key={data.name}>{data.name}</li>
-//               }))}
-//             </ul>
-//         </div>
-        
-//         <div className="row">
-//         {store.list.map((data, index) => (
-//           <div className='col-6 col-md-3'>
-            
-//           <TanList 
-//             keyProps={index}
-//             indexProps={index}
-//             nameProps={data.name}
-//           />
-//           </div>
-//         ))}
-//         </div>
-//       </div>
-//     </AppModalOverlay>
-//   </React.Fragment>
-// )
-
-// import React, {useState} from 'react';
-// import axios from 'axios';
-// import {AppModalOverlay} from './LandingStyle'
-// import mainLogo from '../../assets/mainLogo.png'
-// import appName from '../../assets/appName.png'
-// import TanList from "../tanList/TanList";
-
-
-// const Landing = () => {
-//   const [pagedata, setPagedata] = useState([]);
-//   const [query, setQuery] = useState(" ");
-//   const [store, setStore] = useState({query: ' ', list: []});
-
-//   axios.get("https://pokeapi.co/api/v2/pokemon/").then((response) =>{
-//       console.log(pagedata);
-//       setPagedata(response.data.results);
-//     })
-
-//     // Search function
-//   const searchHandler = (e) => {
-//     const result = tans.filter(data => {
-//       if(e.target.value === " ") return tans
-//       return data.nameProps.toLowerCase().includes(e.target.value.toLowerCase())
-//       })
-//       setStore({
-//           query: e.target.value,
-//           list: results
-//       })
-//   } 
-
-//   return (
-//     <React.Fragment>
-//       <AppModalOverlay>
-//         <div className='modal-container'>
-//           <div className='head-section'>
-//           <div className='app-header'>
-//             <img src={mainLogo} className="app-logo"/> 
-//             <img src={appName} className="app-name"/> 
-//           </div>
-//           {/* <label>Enter Name or Number</label> */}
-//           <div className='poketan-search'>
-//           <input 
-//             type="search" 
-//             placeholder='Pokedex Name or Number' 
-//             value={store.query}
-//             onChange={searchHandler}
-//           />
-//           <button 
-//           // onClick={searchHandler}
-//           >Search</button>
-//           </div>
-//           </div>
-//           <div className='search-result'>
-//               <ul>
-//                 {(store.query === " " ? "No match found!" : !store.list.length ? "Your search did not return any result" : store.list.map)}
-//               </ul>
-//           </div>
-//           <div className="row">
-//           {pagedata.map((data, index) => (
-//             <div className='col-6 col-md-3'>
-              
-//             <TanList 
-//               keyProps={index}
-//               indexProps={index}
-//               nameProps={data.name}
-//             />
-//             </div>
-//           ))}
-//           </div>
-//         </div>
-//       </AppModalOverlay>
-//     </React.Fragment>
-//   )
-// }
-
-// export default Landing;
-
--- src/components/tanList/TanList.jsx
+++ src/components/tanList/TanList.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-// import axios from 'axios';
 import {TanListWrap} from '../tanList/TanListStyle';
 import tan1 from '../../assets/tan1.png'
 
@@ -9,7 +8,6 @@ const TanList = (props) => {
       <TanListWrap>
         <div className="">
         <div className=''>
-          {/* {props.map((data, index) => ())} */}
           <div className='' key={props.keyProps}>
             <div className='tan-card'>
                 <div className='tan-image'>
@@ -32,46 +30,3 @@ const TanList = (props) => {
 }
 
 export default TanList
-
-
-// import React, { useState } from 'react';
-// import axios from 'axios';
-// import {TanListWrap} from '../tanList/TanListStyle';
-// import tan1 from '../../assets/tan1.png'
-
-// const TanList = () => {
-//   const [pagedata, setPagedata] = useState([]);
-
-//   axios.get("https://pokeapi.co/api/v2/pokemon/").then((response) =>{
-//       console.log(pagedata);
-//       setPagedata(response.data.results);
-//     })
-  
-//   return (
-//     <React.Fragment>
-//       <TanListWrap>
-//         <div className="">
-//         <div className='row'>
-//           {pagedata.map((data, index) => (
-//             <div className='col-6 col-md-3' key={index}>
-//             <div className='tan-card'>
-//                 <div className='tan-image'>
-//                   <img src={tan1}/>
-//                 </div>
-//                 <div className='tan-number'>#{index}</div>
-//                 <div className='tan-name'>{data.name}</div>
-//                 <div className='card-button'>
-//                     <button className='button-1'>Grass</button>
-//                     <button className='button-2'>Poison</button>
-//                 </div>
-//             </div>
-//           </div>
-//           ))}          
-//         </div>
-//         </div>
-//       </TanListWrap>
-//     </React.Fragment>
-//   )
-// }
-
-// export default TanList
